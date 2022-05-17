184b24021cd482461405dcefae3c3d55ba376dac
HanningtoneM <hanningtonepompyy@gmail.com>
2022-05-17 16:31:26 +0300

    Changes to image

--- js/speakers.js
+++ js/speakers.js
@@ -5,42 +5,42 @@ const speakers = [
     name: 'Magret Kenyatta',
     position: 'first lady of kenya',
     description: 'Margaret Gakuo Kenyatta is a Kenyan educator, First Lady of Kenya Since 2013, and wife of Uhuru Muigai Kenyatta, fourth president. She is also the founder of beyond-Zero campaign that has helped many women in our country',
-    image: '/images/women/kenyatta.jpg',
+    image: 'images/women/kenyatta.jpg',
   },
 
   {
     name: 'Julie Gichuru',
     position: 'Media personality',
     description: 'Julie Gathoni Sumira Gichuru is a Kenyan entrepreneur and media personality with investments in media, fashion retail and entertainment sectors. She is an independent director at Acumen Communication Limited and is a fellow and trustee of the Africa Leadership Initiative EA.',
-    image: '/images/women/julie.jpg',
+    image: 'images/women/julie.jpg',
   },
 
   {
     name: 'Flora Mworoa',
     position: 'County Comissioner',
     description: 'Flora Mworoa is the County Comissioner of Nairobi county. She grew up in a small town in Meru county and has managed to grow her craft in both education and career wise. She is a definiton of a great career woman',
-    image: '/images/cc2.jpeg',
+    image: 'images/cc2.jpeg',
   },
 
   {
     name: 'Lupita Nyongo',
     position: 'Actor',
     description: 'Lupita Amondi is a Kenyan-Mexican actress. She was born in Mexico City, where her father was teaching, and was raised in Kenya from the age of one.',
-    image: '/images/women/lupita.webp',
+    image: 'images/women/lupita.webp',
   },
 
   {
     name: 'Amina Mohammed',
     position: 'Cabinet Secretary',
     description: 'Kenyan cabinet secretary, lawyer and a diplomat of Somali descent. She is presently serving as the Cabinet Secretary for Sports, Heritage and Culture in Kenya',
-    image: '/images/women/Amina_Mohamed.jpg',
+    image: 'images/women/Amina_Mohamed.jpg',
   },
 
   {
     name: 'Wendo Githaka',
     position: 'Student counsel',
     description: 'student counsel at Universty of Nairobi and also Student mentor. Studies law and has specialized in public speaking. She is also the chairlady of the creating and performing arts club.',
-    image: '/images/wendo.jpeg',
+    image: 'images/wendo.jpeg',
   },
 ];
 
@@ -48,7 +48,7 @@ speakers.forEach((item) => {
   const card = `
       <div class="d-flex featured-left">
         <div class="image">
-          <img src="/images/Tiles.svg" alt="" class="image-left" />
+          <img src="images/Tiles.svg" alt="" class="image-left" />
           <img
             src="${item.image}"
             alt="Magret"
